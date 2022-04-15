a7a126cb416013f2eaae74772976973352afeb4a
igordami <igordamiao.contato@outlook.com>
2022-04-15 20:08:44 -0300

    criando class livro

--- src/br/com/dio/FirstProject.java
+++ src/br/com/dio/FirstProject.java
@@ -5,8 +5,10 @@ import sun.security.mscapi.CPublicKey;
 
 public class FirstProject {
     public static void main(String[] args) {
-        gato gato= new gato();
+        gato gato = new gato();
+        livro livro1 = new livro("Victor Hugo", "1500");
         System.out.println(gato);
+        System.out.println(livro1);
         /*int a,b;
         a=5;
         b=3;
@@ -14,5 +16,41 @@ public class FirstProject {
 */
 
     }
+
+    public static class livro {
+        private String autor;
+        private String npag;
+
+        public livro(String autor, String npag) {
+            this.autor = autor;
+            this.npag = npag;
+        }
+
+        public String getAutor() {
+            return autor;
+        }
+
+        public void setAutor(String autor) {
+            this.autor = autor;
+        }
+
+        public String getNpag() {
+            return npag;
+        }
+
+        public void setNpag(String npag) {
+            this.npag = npag;
+        }
+
+        @Override
+        public String toString() {
+            return "livro{" +
+                    "autor='" + autor + '\'' +
+                    ", npag=" + npag +
+                    '}';
+        }
+    }
 }
+
+
 
